865de69b2a7428d8006c29bed0ea6c6ef4f39899
hunyoung529 <133857185+hunyoung529@users.noreply.github.com>
2024-03-29 15:41:49 +0900

    v

--- src/app/components/SearchUser.tsx
+++ src/app/components/SearchUser.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { FormEvent } from "react";
 
 function SearchUser() {
+  const router = useRouter();
+  console.log(router);
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const userId = (
+      e.currentTarget.elements.namedItem("userId") as HTMLInputElement
+    ).value;
+    // router.push(`/record/${userId}`);
+  };
   return (
     <div
       className="bg-black rounded-lg shadow p-6 w-full flex items-center justify-center"
@@ -8,10 +20,11 @@ function SearchUser() {
         minWidth: 640,
       }}
     >
-      <form className="flex items-center space-x-2">
+      <form className="flex items-center space-x-2" onSubmit={onSubmit}>
         <input
           className=" text-black"
           type="text"
+          name="userId"
           placeholder="Enter user ID"
           required
         />
---
+++ src/app/pages/record/[id].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+function RecordPage() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default RecordPage;
